6f9b8c7e9f853d9c9ac9182aa934eaf1de4b404f
Rafael Pinheiro <124298994+mullenleafar@users.noreply.github.com>
2023-06-05 12:29:39 -0300

    Colocando algumas peças no tabuleiro (teste)

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,10 +5,7 @@ import chess.ChessMatch;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		ChessMatch match = new ChessMatch();
 		UI.printBoard(match.getPieces());
-
 	}
-
 }
