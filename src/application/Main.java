efe37e66c8321de9c8a1cf0bd6749da729af4bf9
Rafael Pinheiro <124298994+mullenleafar@users.noreply.github.com>
2023-06-06 14:57:15 -0300

    Implementando revezamento de turnos.

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(match.getPieces());
+				UI.printMatch(match);
 				
 				System.out.println();
 				System.out.print("Source: ");
--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -49,6 +50,13 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error while reading chess position. Valid values are from a1 to h8.");
 		}
+	}
+	
+	public static void printMatch(ChessMatch match) {
+		printBoard(match.getPieces());
+		System.out.println();
+		System.out.println("Turn " + match.getTurn());
+		System.out.println("Waiting player: " + match.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
