2938f3a03e4dce08e552a0f7ee5709c248becad5
Rafael Pinheiro <124298994+mullenleafar@users.noreply.github.com>
2023-06-06 14:18:52 -0300

    Movimentos possíveis (rei).

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,65 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	public boolean canMove(Position p) {
+		ChessPiece piece = (ChessPiece)getBoard().piece(p);
+		return piece == null || piece.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		
+		//Cima
+		p.setValues(this.position.getRow() - 1, this.position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Cima-direita
+		p.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Direita
+		p.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Baixo-direita
+		p.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Baixo
+		p.setValues(this.position.getRow() + 1, this.position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Baixo-esquerda
+		p.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Esquerda
+		p.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Cima-esquerda
+		p.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
 		return mat;
 	}
 
